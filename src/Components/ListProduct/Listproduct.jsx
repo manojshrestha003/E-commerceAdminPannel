e5cbe262362ae44660fd3ae9b86d2e798228e7d1
manojshrestha003 <manoj.shrestha2081@gmail.com>
2024-12-21 19:34:31 +0545

    admin pannel update

--- src/Components/ListProduct/Listproduct.jsx
+++ src/Components/ListProduct/Listproduct.jsx
@@ -1,58 +1,71 @@
-import React, { useEffect, useState } from 'react'
-import './Listproduct.css'
-import cross_icon from '../../assets/cross_icon.png'
+import React, { useEffect, useState } from 'react';
+import './Listproduct.css';
+import cross_icon from '../../assets/cross_icon.png';
+
 const Listproduct = () => {
   const [allProducts, setAllProducts] = useState([]);
 
-  const fetchInfo = async () =>{
-    await fetch('http://localhost:4000/allproducts').then((res)=>res.json())
-    .then((data)=>{setAllProducts(data)});
-  }
-  useEffect(()=>{
+  const fetchInfo = async () => {
+    await fetch('http://localhost:4000/allproducts')
+      .then((res) => res.json())
+      .then((data) => {
+        setAllProducts(data);
+      });
+  };
+
+  useEffect(() => {
     fetchInfo();
-  }, [])
-  const removeProduct =async(id)=>{
-    await fetch('http://localhost:4000/removeproduct',{
+  }, []);
+
+  const removeProduct = async (id) => {
+    await fetch('http://localhost:4000/removeproduct', {
       method: 'POST',
-      headers:{
+      headers: {
         Accept: 'application/json',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify({id:id})
-    })
-    await fetchInfo()
-  }
+      body: JSON.stringify({ id: id }),
+    });
+    await fetchInfo();
+  };
+
   return (
-    <div className='list-product'>
-      
-     <h1>All products list </h1>
-     <div className="lsitProduct-format-main">
-      <p>Products</p>
-      <p>Title</p>
-      <p>Old Price</p>
-      <p>New Price</p>
-      <p>Category</p>
-      <p>Remove</p>
-     </div>
-     <div className="lsit-product-allProduct">
-      <hr />
-      {allProducts.map((product, index)=>{
-        return <><div key={index} className="lsitProduct-format-main listproduct-format">
-          <img className='listproduct-product-icon ' src={product.image} alt="" />
-          <p>{product.name}</p>
-          <p>${product.old_price}</p>
-          <p>${product.new_price}</p>
-          <p>{product.category}</p>
-          <img onClick={()=>{removeProduct(product.id)}} src={cross_icon} alt="" className='listproduct-remove-icon' />
-
-        </div>
+    <div className="list-product">
+      <h1>All products list</h1>
+      <div className="lsitProduct-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Old Price</p>
+        <p>New Price</p>
+        <p>Category</p>
+        <p>Remove</p>
+      </div>
+      <div className="lsit-product-allProduct">
         <hr />
-        </>
-      })}
+        {allProducts.map((product, index) => {
+  return (
+    <React.Fragment key={product.id || index}>
+      <div className="lsitProduct-format-main listproduct-format">
+        <img className='listproduct-product-icon' src={product.image} alt="" />
+        <p>{product.name}</p>
+        <p>${product.old_price}</p>
+        <p>${product.new_price}</p>
+        <p>{product.category}</p>
+        <img 
+          onClick={() => { removeProduct(product.id); }} 
+          src={cross_icon} 
+          alt="" 
+          className='listproduct-remove-icon' 
+        />
+      </div>
+      <hr />
+    </React.Fragment>
+  );
+})}
 
-     </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Listproduct
+export default Listproduct;
